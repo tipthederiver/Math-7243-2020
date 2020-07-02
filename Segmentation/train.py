63b2607f77c711d47f6dbdf87023273ef5ee7d62
tipthederiver <43258590+tipthederiver@users.noreply.github.com>
2020-07-02 15:28:24 -0400

    Update train.py

--- Segmentation/train.py
+++ Segmentation/train.py
@@ -5,8 +5,19 @@ import argparse
 
 parser = argparse.ArgumentParser(
                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+# Data and model checkpoints directories					
+parser.add_argument('--model_dir', type=str, default='save',
+                    help='directory to store checkpointed models')
                     
 # Data and model checkpoints directories
 parser.add_argument('--data_dir', type=str, default='data/tinyshakespeare',
                     help='data directory containing input.txt with training examples')     
                     
+args = parser.parse_args()
+files = os.listdir(args.data_dir)
+print(files)
+
+save_path = os.path.join(args.model_dir, "Output.txt")
+with open(save_path, "w") as text_file:
+    print(f"Purchase Amount: {files}", file=text_file)
