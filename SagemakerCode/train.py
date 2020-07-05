32679448107a2f9f5c87e2d86f82bb7d4baf41a3
tipthederiver <43258590+tipthederiver@users.noreply.github.com>
2020-07-05 17:12:37 -0400

    Update train.py

--- SagemakerCode/train.py
+++ SagemakerCode/train.py
@@ -46,6 +46,8 @@ print("sucess!")
 model = md.unet2D(input_size = (512,512,4))
 history = md.train_model(model, train_gen, test_gen, name="model", checkpoint_dir=args.final_model, epochs=2)
 
+print(args.final_model)
+
 model.save(args.final_model + '/trainedmodel.h5') # saving the model
 with open(args.final_model + '/trainHistoryOld', 'wb') as handle: # saving the history of the model
-    dump(history.history, handle)
+    pickle.dump(history.history, handle)
